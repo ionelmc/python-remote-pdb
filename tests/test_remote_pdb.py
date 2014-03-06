80c13ab4fe01d7e28f1d4ecb4543e0cfee55d473
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-03-07 01:30:09 +0200

    Add tests.

--- tests/test_remote_pdb.py
+++ tests/test_remote_pdb.py
@@ -19,13 +19,17 @@ from remote_pdb import set_trace
 #aspectlib.weave(
 #    ('socket.socket', 'socket._fileobject'),
 #    aspectlib.debug.log(stacktrace=6), # show 6 frames
-#    on_init=True
+#    on_init=True,
+#    only_methods=['read', 'write']
 #)
 
 TIMEOUT = int(os.getenv('REMOTE_PDB_TEST_TIMEOUT', 10))
 
 
 class RemotePDBTestCase(ProcessTestCase):
+    def assertStartsWith(self, string, start):
+        self.assertTrue(string.startswith(start), '%r does not start with %r' % (string, start))
+
     def test_simple(self):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_simple') as proc:
             with self.dump_on_error(proc.read):
@@ -50,6 +54,42 @@ class RemotePDBTestCase(ProcessTestCase):
                     'Restoring streams',
                     'DIED.',
                 )
+                #print('\nCHILD> '.join(proc.read().splitlines()))
+
+    def test_redirect(self):
+        with TestProcess(sys.executable, __file__, 'daemon', 'test_redirect') as proc:
+            with self.dump_on_error(proc.read):
+                self.wait_for_strings(proc.read, TIMEOUT,
+                    '{a1}',
+                    '{b1}',
+                    'RemotePdb session open at ',
+                )
+                host, port = re.findall("RemotePdb session open at (.+):(.+),", proc.read())[0]
+                with closing(socket.create_connection((host, int(port)), timeout=TIMEOUT)) as conn:
+                    if PY3:
+                        fh = conn.makefile('rw', buffering=1)
+                    else:
+                        fh = conn.makefile(bufsize=0)
+                    self.wait_for_strings(proc.read, TIMEOUT, 'accepted connection from')
+                    fh.readline()
+                    self.assertEqual("-> print('{b2}')", fh.readline().strip())
+                    fh.write('break func_a\r\n')
+                    fh.write('continue\r\n')
+                    self.assertStartsWith(fh.readline(), '(Pdb) Breakpoint')
+                    self.assertEqual(fh.readline().strip(), '(Pdb) {b2}')
+                    fh.readline()
+                    self.assertEqual("-> print('{a2}')", fh.readline().strip())
+                    fh.write('continue\r\n')
+                    try:
+                        fh.readline()
+                    except Exception as exc:
+                        print("fh.readline() failed:", exc)
+
+                self.wait_for_strings(proc.read, TIMEOUT,
+                    'DIED.',
+                )
+                self.assertNotIn('Restoring streams', proc.read())
+                #print('\nCHILD> '.join(proc.read().splitlines()))
 
     def test_simple_break(self):
         with TestProcess(sys.executable, __file__, 'daemon', 'test_simple') as proc:
@@ -83,16 +123,17 @@ class RemotePDBTestCase(ProcessTestCase):
                     'DIED.',
                 )
                 self.assertNotIn('Restoring streams', proc.read())
+                #print('\nCHILD> '.join(proc.read().splitlines()))
 
 
-def func_b():
+def func_b(patch_stdstreams):
     print('{b1}')
-    set_trace()
+    set_trace(patch_stdstreams=patch_stdstreams)
     print('{b2}')
 
-def func_a(block=lambda _: None):
+def func_a(block=lambda _: None, patch_stdstreams=False):
     print('{a1}')
-    func_b()
+    func_b(patch_stdstreams)
     print('{a2}')
     x = block('{a3} ?')
     print('{=> %s}' % x)
@@ -110,8 +151,8 @@ if __name__ == '__main__':
 
         if test_name == 'test_simple':
             func_a()
-        elif test_name == 'test_?':
-            pass
+        elif test_name == 'test_redirect':
+            func_a(patch_stdstreams=True)
         else:
             raise RuntimeError('Invalid test spec %r.' % test_name)
         logging.info('DIED.')
